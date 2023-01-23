16dc9aac0b0556aca92ec4f1de64664a6bd02976
Roberto Dab <roberto.dabb@gmail.com>
2023-01-23 16:31:37 +0100

    Created Species Controller

---
+++ ClinicaVeterinariaApp/Controllers/SpeciesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClinicaVeterinariaApp.Models;
+
+namespace ClinicaVeterinariaApp.Controllers
+{
+    public class SpeciesController : Controller
+    {
+        private ModelDBContext db = new ModelDBContext();
+
+        // GET: Species
+        public ActionResult Index()
+        {
+            return View(db.Species.ToList());
+        }
+
+        // GET: Species/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Species species = db.Species.Find(id);
+            if (species == null)
+            {
+                return HttpNotFound();
+            }
+            return View(species);
+        }
+
+        // GET: Species/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="SpecieID , Specie")] Species newSpecie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Species.Add(newSpecie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(newSpecie);
+        }
+
+
+
+        // GET: Species/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Species species = db.Species.Find(id);
+            if (species == null)
+            {
+                return HttpNotFound();
+            }
+            return View(species);
+        }
+
+        // POST: Species/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SpecieID,Specie")] Species species)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(species).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(species);
+        }
+
+        // GET: Species/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Species species = db.Species.Find(id);
+            if (species == null)
+            {
+                return HttpNotFound();
+            }
+            return View(species);
+        }
+
+        // POST: Species/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Species species = db.Species.Find(id);
+            db.Species.Remove(species);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
