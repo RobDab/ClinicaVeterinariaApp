eedee1c2c0d9ab2c4044009e3b8c8b33cd509d38
Roberto Dab <roberto.dabb@gmail.com>
2023-01-25 16:12:28 +0100

    Created tool class AnimalsJSON for AJAX requests

---
+++ ClinicaVeterinariaApp/Models/AnimalsJSON.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicaVeterinariaApp.Models
+{
+    public class AnimalsJSON
+    {
+        public int IDAnimal { get; set; }
+
+        public string RegisterDate { get; set; }
+
+        public string Name { get; set; }
+
+        public int SpecieID { get; set; }
+
+        public string Color { get; set; }
+
+        public string ChipNumber { get; set; }
+
+        public bool HasOwner { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string OwnerLastname { get; set; }
+
+        public string UrlPhoto { get; set; }
+    }
+}
