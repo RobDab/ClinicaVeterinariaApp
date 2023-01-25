763decf878162a96e050699e554bc8bcb85faa4a
Lambo95 <lamberto.martini95@gmail.com>
2023-01-25 12:21:18 +0100

    update

--- ClinicaVeterinariaApp/Controllers/HomeController.cs
+++ ClinicaVeterinariaApp/Controllers/HomeController.cs
@@ -21,22 +21,24 @@ namespace ClinicaVeterinariaApp.Controllers
             return View();
         }
 
-        public JsonResult GetAnimal(string strChip) {
+        public JsonResult GetAnimal(string strChip)
+        {
             try
             {
                 Animals animal = db.Animals.Where(a => a.ChipNumber == strChip).FirstOrDefault();
-                if(animal != null) { 
-                Animals animalToReturn = new Animals
+                if (animal != null)
                 {
-                    Name = animal.Name,
-                    Color = animal.Color,
-                    HasOwner = animal.HasOwner,
-                    UrlPhoto= animal.UrlPhoto,
-                    BirthDate= animal.BirthDate,
-                    RegisterDate= animal.RegisterDate,
+                    Animals animalToReturn = new Animals
+                    {
+                        Name = animal.Name,
+                        Color = animal.Color,
+                        HasOwner = animal.HasOwner,
+                        UrlPhoto = animal.UrlPhoto,
+                        BirthDate = animal.BirthDate,
+                        RegisterDate = animal.RegisterDate,
 
-                };
-                return Json(animalToReturn, JsonRequestBehavior.AllowGet);
+                    };
+                    return Json(animalToReturn, JsonRequestBehavior.AllowGet);
                 }
                 else
                 {
@@ -50,5 +52,50 @@ namespace ClinicaVeterinariaApp.Controllers
             }
             finally { }
         }
-    }
+
+        public ActionResult SearchAnimals()
+        {
+            ViewBag.SpecieList = db.Species.ToList();
+            return View();
+        }
+
+        public JsonResult GetAnimals(string ArrIdParam)
+        {
+            try
+            {
+
+                List<Animals> ListAnimal = new List<Animals>();
+
+                foreach (char SpecieID in ArrIdParam)
+                {
+                    int SpecieIDInt = (int)Char.GetNumericValue(SpecieID);
+                    List<Animals> animals = db.Animals.Where(a => a.SpecieID == SpecieIDInt && a.HasOwner == false).ToList();
+
+                    foreach (var animal in animals)
+                    {
+                        ListAnimal.Add(animal);
+                    }
+                }
+
+                
+
+
+                if (ListAnimal.Count() != 0)
+                {
+
+                    return Json(ListAnimal, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    ViewBag.JsonErr = "Animali non trovati!";
+                    return Json(null, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+            finally { }
+        }
+}
 }
